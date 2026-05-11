27ef21fdf6741aa056a502e32034764b13dae24d
samuelmel <samuca21santos@gmail.com>
2026-05-11 17:55:57 -0300

    comecando criacao banco de dados

---
+++ app/bancodedados.py
@@ -0,0 +1,26 @@
+import sqlite3
+import os
+
+DB_PATH = os.path.join("database", "estoque.db")
+
+def conectar():
+    return sqlite3.connect(DB_PATH)
+
+
+def criar_tabela():
+    os.makedirs(os.path.dirname(DB_PATH), exist_ok=True)
+
+
+    conexao = conectar()
+    cursor = conexao.cursor()
+
+
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS produtos (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            nome TEXT NOT NULL,
+            quantidade INTEGER NOT NULL,
+            preco REAL NOT NULL
+        )
+    ''')
+    
